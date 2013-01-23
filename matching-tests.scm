32714e1305d8383976f6689cd68785716e33b1c9
Alexey Radul <axch@mit.edu>
2013-01-23 16:32:54 -0500

    License headers.

--- matching-tests.scm
+++ matching-tests.scm
@@ -1,3 +1,21 @@
+;;; This file is part of Pattern Case, a Schemely pattern matching
+;;; case facility in MIT Scheme.
+;;; Copyright 2011 Alexey Radul.
+;;;
+;;; Pattern Case is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Affero General Public License
+;;; as published by the Free Software Foundation; either version 3 of
+;;; the License, or (at your option) any later version.
+;;; 
+;;; Pattern Case is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Pattern Case; if not, see
+;;; <http://www.gnu.org/licenses/>.
+
 
 (case*
  thing
